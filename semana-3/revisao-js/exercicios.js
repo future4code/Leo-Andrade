df88ecd212018395a0ec5b9845b61e832f2d8960
Leo Ramos Andrade <leo.ramos182@hotmail.com>
2021-08-23 11:06:37 -0300

    feito o exercicio 4

--- semana-3/revisao-js/exercicios.js
+++ semana-3/revisao-js/exercicios.js
@@ -38,7 +38,13 @@ function retornaArrayOrdenado(array) {
 
 // EXERCÍCIO 04
 function retornaNumerosPares(array) {
-  
+    const numerosPares = []
+  for(let i = 0 ; i < array.length ; i++){
+      if(array[i] % 2 === 0){
+        numerosPares.push(array[i])
+      }
+  }
+  return numerosPares
 }
 
 // EXERCÍCIO 05
