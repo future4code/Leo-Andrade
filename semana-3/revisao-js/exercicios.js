db82793b006f372ae7c3f22228e3e256f95e3701
Leo Ramos Andrade <leo.ramos182@hotmail.com>
2021-08-23 11:03:28 -0300

    feito o exercicio 3

--- semana-3/revisao-js/exercicios.js
+++ semana-3/revisao-js/exercicios.js
@@ -21,7 +21,19 @@ function retornaArrayInvertido(array) {
 
 // EXERCÍCIO 03
 function retornaArrayOrdenado(array) {
-  
+    const arrayOrdenado = []
+    let maior = 0
+    let menor = 0
+    for(let i = 0 ; i < array.length ; i++){
+        for (let j = i + 1 ; j < array.length; j++){
+            if(array[j] < array[i]){
+                menor = array[j]
+                array[j] = array[i]
+                array[i] = menor
+            }
+        }
+    }
+    return array
 }
 
 // EXERCÍCIO 04
